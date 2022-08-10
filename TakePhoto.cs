3382521c5c9af86dccbd592eb519f62de1ae3028
tomishninja <tomishninja@users.noreply.github.com>
2022-08-10 16:17:16 +0930

    Added Take Photo Class to Repo

---
+++ TakePhoto.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class TakePhoto : MonoBehaviour
+{
+    public bool takePhoto = false;
+    public string photoName = "Screenshot.png";
+    public int superSize = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (takePhoto)
+        {
+            ScreenCapture.CaptureScreenshot(photoName, superSize);
+            takePhoto = false;
+        }
+    }
+}
